7d398c52a41e5d813d2df5313649895e37c02f79
Steven Martinez <steven-0198@hotmail.com>
2023-05-10 16:30:24 -0500

    mjmm

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 
-app.get('/status',(req,res) => res.send('sapo'))
+app.get('/status',(req,res) => res.send('sapoooo'))
 
 // listen
 app.listen(PORT)
