073bfb246eb6a4f9e89e24a16ebcb154c8baee78
Steven Martinez <steven-0198@hotmail.com>
2023-05-11 08:09:53 -0500

    ssa

--- src/router/login.js
+++ src/router/login.js
@@ -1,26 +1,26 @@
-// const express = require("express");
-// // database
-// const db = require("../connect/connection.js");
-// // utils
-// const utils = require("../utils/utils.js");
-// // login router const
-// const loginRouter = express.Router();
+const express = require("express");
+// database
+const db = require("../connect/connection.js");
+// utils
+const utils = require("../utils/utils.js");
+// login router const
+const loginRouter = express.Router();
 
-// loginRouter.post("/", async (req, res) => {
-//   const { body } = req;
-//   try {
-//     if (!body.username || !body.password)
-//       utils.errorReponse(res, 204, "Correo y/o contraseña requeridas");
-//     const query = `SELECT id, username, rol FROM access WHERE username="${body.username}" and password="${body.password}"`;
-//     const data = await db.handleQuery(query);
-//     utils.sucessResponse(
-//       res,
-//       data,
-//       data?.length ? "success" : "no existe el usuario"
-//     );
-//   } catch (e) {
-//     utils.errorReponse(res, 500, "Error en la conexión a la base de datos");
-//   }
-// });
+loginRouter.post("/", async (req, res) => {
+  const { body } = req;
+  try {
+    if (!body.username || !body.password)
+      utils.errorReponse(res, 204, "Correo y/o contraseña requeridas");
+    const query = `SELECT id, username, rol FROM access WHERE username="${body.username}" and password="${body.password}"`;
+    const data = await db.handleQuery(query);
+    utils.sucessResponse(
+      res,
+      data,
+      data?.length ? "success" : "no existe el usuario"
+    );
+  } catch (e) {
+    utils.errorReponse(res, 500, "Error en la conexión a la base de datos");
+  }
+});
 
-// module.exports = loginRouter;
+module.exports = loginRouter;
