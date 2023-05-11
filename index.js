b682382e05214c703d2c8f005047d203adc243c8
Steven Martinez <steven-0198@hotmail.com>
2023-05-11 07:58:19 -0500

    tes

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // CONST
 const PORT = 3001;
 
 // // import routers
-// const loginRouter = require('./src/router/login');
-// const uploadRouter = require('./src/router/upload');
-// const usersRouter = require('./src/router/users');
-// const productsRouter = require('./src/router/products');
-// const remissionRouter = require('./src/router/remission');
-// const pdfRouter = require('./src/router/pdf');
-// const boxRouter = require('./src/router/box');
-// const boxMovementRouter = require('./src/router/box_movement');
+const loginRouter = require('./src/router/login');
+const uploadRouter = require('./src/router/upload');
+const usersRouter = require('./src/router/users');
+const productsRouter = require('./src/router/products');
+const remissionRouter = require('./src/router/remission');
+const pdfRouter = require('./src/router/pdf');
+const boxRouter = require('./src/router/box');
+const boxMovementRouter = require('./src/router/box_movement');
 
 // // import middleware
 // const middlewareHeaders = require('./src/middlewares/headers');
